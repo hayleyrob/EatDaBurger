c6d8d69852dd5ee49ce7305c99d854f100e82b39
Hayley Robinson <hayleyrobinson328@gmail.com>
2020-06-14 16:09:07 -0500

    updated links

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,4 @@
 USE burgers_db;
+
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Famous Star", false ), ("Big Mac", false), ("Double Double", false), ("Quarter Pounder", false), ("Miracle Meat Burger", false);
